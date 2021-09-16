cc15f90603d0a0d95e34825ae5c685a2d3d64c79
Jonty Wareing <jonty@jonty.co.uk>
2021-09-16 12:52:30 +0100

    Image fetcher

---
+++ fetch-openhouse-images.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import os
+import sys
+import json
+import urllib
+
+from github import Github
+
+year = 2021
+
+g = Github(os.environ["GITHUB_TOKEN"])
+repo = g.get_repo("jonty/open-house-london-data")
+contents = repo.get_contents("data/%s" % year)
+for content_file in contents:
+    data = json.loads(content_file.decoded_content)
+    print("\nChecking %s images for location %s..." % (len(data["images"]), data["id"]))
+
+    path = "images/%s/%s" % (year, data["id"])
+    os.makedirs(path, exist_ok=True)
+
+    for image in data["images"]:
+        if image["url"].startswith("/"):
+            print("SKIPPING INVALID IMAGE: %s" % image)
+            continue
+
+        filename = os.path.basename(image["url"])
+        file_path = "%s/%s" % (path, filename)
+
+        if not os.path.isfile(file_path):
+            print("Downloading %s" % image["url"])
+            urllib.request.urlretrieve(image["url"], file_path)
